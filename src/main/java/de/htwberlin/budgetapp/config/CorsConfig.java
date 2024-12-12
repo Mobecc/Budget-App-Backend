44f80be6ce8479176561933208a60c2b6c795c3b
Mehdi030 <DEINE_EMAIL>
2024-12-12 18:05:44 +0100

    Neue Frontend URL im CorsConfig angepasst

--- src/main/java/de/htwberlin/budgetapp/config/CorsConfig.java
+++ src/main/java/de/htwberlin/budgetapp/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 // Erlaube alle Endpunkte
                 registry.addMapping("/**")
-                        .allowedOrigins("https://budget-app-frontend-1umt.onrender.com", "http://localhost:3000") // Erlaube Frontend-URLs (Render und Local)
+                        .allowedOrigins("https://budget-app-frontend-tpct.onrender.com") // Erlaube Frontend-URLs (Render und Local)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Erlaubte HTTP-Methoden
                         .allowedHeaders("*") // Alle Header erlauben
                         .allowCredentials(true) // Cookies/Authtoken erlauben
